1d14f454aa23044841ae825f5b6590449cb64f30
Im_optimus <84771451+Zaifmohammed@users.noreply.github.com>
2021-09-05 13:13:55 +0400

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,27 +26,8 @@ from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, In
 from pyrogram import Client, errors
 from config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
-buttons = [
-    [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-        InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
-    ],
-    [
-        InlineKeyboardButton('🎧Play Music', url=f'https://t.me/{USERNAME}'),
-        InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help')       
-    ]
-    ]
-@Client.on_inline_query()
-async def search(client, query):
-    answers = []
-    if query.query == "ORU_MANDAN_PM_VANNU":
-        answers.append(
-            InlineQueryResultArticle(
-                title="Deploy",
-                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/MusicPlayer) below.</b>", disable_web_page_preview=True),
-                reply_markup=InlineKeyboardMarkup(buttons)
-                )
-            )
+
+
         await query.answer(results=answers, cache_time=0)
         return
     string = query.query.lower().strip().rstrip()
